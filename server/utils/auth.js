dd8e1fe32afb8719ace26c19c98effb266d20af6
parappa20 <88298540+james182-dang@users.noreply.github.com>
2022-01-09 18:23:26 -0600

    update apollo

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,10 +5,17 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -18,6 +25,7 @@ module.exports = {
         .trim();
     }
 
+    // if no token, send back error
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
@@ -33,10 +41,5 @@ module.exports = {
 
     // send to next endpoint
     next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
 };
