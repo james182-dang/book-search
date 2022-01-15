00865c37da41929a2554a496c9682ce976c3d81a
parappa20 <88298540+james182-dang@users.noreply.github.com>
2022-01-15 14:05:26 -0600

    add addUser and login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,32 @@ const resolvers = {
 
             throw new AuthenticationError('Not logged in');
         },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user};
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials!');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
 
         savedBooks: async (parent, { username }) => {
             const params = username ? { username } : {},
@@ -34,10 +60,6 @@ const resolvers = {
               .select('-__v -password')
               .populate('savedBooks');
         }
-    },
-
-    Mutation: {
-        
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,6 +27,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
+        savedBooks:
     }
 
     type Auth {
